105750b9bfeb511d9ccb95efc780bac3cdaeb9fe
Даник <danilakorotkevich62@gmail.com>
2025-09-29 19:52:28 +0300

    Correction homework Lesson_7_junit_5

--- aqaCourse/src/test/java/org/aston/Lesson_7_junit_5/TestingClassJUnitTest.java
+++ aqaCourse/src/test/java/org/aston/Lesson_7_junit_5/TestingClassJUnitTest.java
@@ -1,4 +1,5 @@
-import org.aston.Lesson_7_junit_5.TestingClass;
+package org.aston.Lesson_7_junit_5;
+
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,27 +14,40 @@ public class TestingClassJUnitTest {
     @Test
     @DisplayName("Тест calcFactorial")
     public void testFactorial() {
+        //тест валидных значений
         assertEquals(1L, testingClass.calcFactorial(0));
         assertEquals(1L, testingClass.calcFactorial(1));
         assertEquals(120L, testingClass.calcFactorial(5));
         assertEquals(3628800L, testingClass.calcFactorial(10));
         assertEquals(2432902008176640000L, testingClass.calcFactorial(20));
+
+        //тест невалидных значений
+        assertThrows(IllegalArgumentException.class, () -> {
+            testingClass.calcFactorial(-1);
+        });
     }
 
     @Test
     @DisplayName("Тест calcSquare")
     public void testCalcSquare() {
-        //тест невалидных значений
-        assertEquals(0, testingClass.calcSquare(0, 5));
-        assertEquals(0, testingClass.calcSquare(10, 0));
         //тест валидных значений
+        assertEquals(13.75, testingClass.calcSquare(5.5, 5));
         assertEquals(25, testingClass.calcSquare(5, 10));
         assertEquals(60, testingClass.calcSquare(15, 8));
+
+        //тест невалидных значений
+        assertThrows(IllegalArgumentException.class, () -> {
+            testingClass.calcSquare(10, 0);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            testingClass.calcSquare(-1, 5);
+        });
     }
 
     @Test
     @DisplayName("Тест calculation")
     public void testCalculation() {
+        //тест валидных значений
         assertEquals(25, testingClass.calculation(10, 15, "+"));
         assertEquals(0, testingClass.calculation(-5, 5, "+"));
 
@@ -48,22 +62,30 @@ public class TestingClassJUnitTest {
         assertEquals(6, testingClass.calculation(-3, -2, "*"));
 
         assertEquals(2, testingClass.calculation(4, 2, "/"));
-        assertEquals(0, testingClass.calculation(7, 3, "/"));
-        assertEquals(0, testingClass.calculation(8, 5, "/"));
-        assertEquals(0, testingClass.calculation(5, 0, "/"));
-        assertEquals(0, testingClass.calculation(2, 10, "/"));
         assertEquals(-2, testingClass.calculation(-10, 5, "/"));
         assertEquals(-5, testingClass.calculation(50, -10, "/"));
+        assertEquals(0, testingClass.calculation(0, 10, "/"));
+
+        //тест невалидных значений
+        assertThrows(IllegalArgumentException.class, () -> {
+            testingClass.calculation(5, 10, "qwe");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            testingClass.calculation(5, 0, "/");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            testingClass.calculation(5, 10, "/");
+        });
     }
 
     @Test
     @DisplayName("Тест equalsNums")
-    public void testEqualsNums() {
-        assertEquals(-1, testingClass.equalsNums(5, 6));
-        assertEquals(0, testingClass.equalsNums(2, 2));
-        assertEquals(1, testingClass.equalsNums(7, 3));
-        assertEquals(0, testingClass.equalsNums(0, 0));
-        assertEquals(1, testingClass.equalsNums(14, 0));
-        assertEquals(-1, testingClass.equalsNums(0, 1));
+    public void testEqualityNums() {
+        assertEquals(-1, testingClass.equalityNums(5, 6));
+        assertEquals(0, testingClass.equalityNums(2, 2));
+        assertEquals(1, testingClass.equalityNums(7, 3));
+        assertEquals(0, testingClass.equalityNums(0, 0));
+        assertEquals(1, testingClass.equalityNums(14, 0));
+        assertEquals(-1, testingClass.equalityNums(0, 1));
     }
 }
